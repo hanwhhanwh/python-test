0dde955282479aeda85625f0b54e6370b14ee472
Wonhee <hanwhhanwh@gmail.com>
2021-03-26 23:26:20 +0900

    add turtle

---
+++ turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.shape('turtle')
+
+turtle.forward(200)
+turtle.right(200)
+turtle.forward(200)
+turtle.right(200)
+turtle.forward(200)
+turtle.right(200)
+turtle.forward(200)
+turtle.right(200)
+
+turtle.done()
