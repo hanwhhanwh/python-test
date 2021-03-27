0ce70e01421513fd9faae51bae6dc806f3a256a3
Wonhee <hanwhhanwh@gmail.com>
2021-03-27 11:30:06 +0900

    add turtle example

---
+++ turtle1.py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+
+t.shape('turtle')
+
+t.forward(200)
+t.right(200)
+t.forward(200)
+t.right(200)
+t.forward(200)
+t.right(200)
+t.forward(200)
+t.right(200)
+
+t.done()
