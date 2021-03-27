9089ff08142dafd4802310586fd5eb278e54cc9d
Wonhee <hanwhhanwh@gmail.com>
2021-03-27 12:10:22 +0900

    turtle draw

--- turtle1.py
+++ turtle1.py
@@ -1,15 +1,34 @@
 import turtle
-t = turtle.Turtle()
+import random
 
-t.shape('turtle')
+def screenLeftClick(x, y):
+	global r, g, b
+	turtle.pencolor((r, g, b))
+	turtle.pendown()
+	turtle.goto(x, y)
 
-t.forward(200)
-t.right(200)
-t.forward(200)
-t.right(200)
-t.forward(200)
-t.right(200)
-t.forward(200)
-t.right(200)
+def screenRightClick(x, y):
+	turtle.penup()
+	turtle.goto(x, y)
 
-t.done()
+def screenMidClick(x, y):
+	global r, g, b
+
+	tSize = random.randrange(1, 10)
+	turtle.shapesize(tSize)
+	r = random.random()
+	g = random.random()
+	b = random.random()
+
+pSize = 10
+r, g, b = 0.0, 0.0, 0.0
+
+turtle.title('거북이로 그림 그리기')
+turtle.shape('turtle')
+turtle.pensize(pSize)
+
+turtle.onscreenclick(screenLeftClick, 1)
+turtle.onscreenclick(screenRightClick, 3)
+turtle.onscreenclick(screenMidClick, 2)
+
+turtle.done()
