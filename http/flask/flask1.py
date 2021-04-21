0a894951e6a374dc6644a789c0899450f8a92438
Wonhee <hanwhhanwh@gmail.com>
2021-04-21 22:26:23 +0900

    add route

--- http/flask/flask1.py
+++ http/flask/flask1.py
@@ -1,9 +1,30 @@
 # 첫 번째 flask 예제
 # reference : 
 from flask import Flask
+from datetime import datetime
+import re
 
 app = Flask(__name__)
+
+print(__name__)
 
 @app.route("/")
 def hello():
 	return "hello, world!"
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
