ec3be666f8e14c6a67e179b75d734ff4ae57249e
Wonhee <hanwhhanwh@gmail.com>
2021-04-26 01:09:44 +0900

    .

--- http/flask/mvc-demo1/app/controllers/__init__.py
+++ http/flask/mvc-demo1/app/controllers/__init__.py
@@ -1,7 +1,17 @@
+from flask import Blueprint
+
 from .user import CreateUser
 from .addr import Addr
 
 
+# RESTful API controllers
 def register_apis(api):
 	api.add_resource(Addr, '/api/v2.0/addr')
 	api.add_resource(CreateUser, '/api/v2.0/user')
+
+
+# normal controllers : by blueprint
+def create_blueprint(name) -> Blueprint:
+    blueprint = Blueprint(name, __name__)
+
+    return blueprint
