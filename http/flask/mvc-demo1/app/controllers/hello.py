00d4739a60dc88ac1e905d59f553d7ad197de8bd
Wonhee <hanwhhanwh@gmail.com>
2021-04-26 17:19:02 +0900

    add hello controller

---
+++ http/flask/mvc-demo1/app/controllers/hello.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template
+from datetime import datetime
+import re
+
+hello = Blueprint('hello', __name__)
+
+@hello.route("/")
+def hello_page():
+    return "hello, world!"
+
+@hello.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
--- http/flask/mvc-demo1/server.py
+++ http/flask/mvc-demo1/server.py
@@ -1,38 +1,16 @@
 # MVC flask 예제
 from flask import Flask
-from datetime import datetime
-import re
 from flask_restful import Api
 from app.controllers import register_apis
+from app.controllers.hello import hello
 
 app = Flask(__name__)
 api = Api(app)
 
+# register RESTful APIs
 register_apis(api)
-
-print(__name__)
-
-@app.route("/")
-def hello():
-	return "hello, world!"
-
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
-
+# register Controllers
+app.register_blueprint(hello)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
