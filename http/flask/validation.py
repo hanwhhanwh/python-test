0d1343bdf0418833b4d27896feff8e9f9b1f10c2
Wonhee <hanwhhanwh@gmail.com>
2021-06-10 21:33:31 +0900

    add validation for json

---
+++ http/flask/validation.py
@@ -0,0 +1,25 @@
+from jsonschema import validate
+
+# A sample schema, like what we'd get from json.load()
+schema = {
+	"type" : "object",
+	"properties" : {
+		"price" : {"type" : "number"},
+		"name" : {"type" : "string"},
+	},
+}
+
+# If no exception is raised by validate(), the instance is valid.
+validate(instance={"name" : "Eggs", "price" : 34.99}, schema=schema)	
+validate(
+	instance={"name" : "Eggs", "price" : "Invalid"}, schema=schema,
+)                                   # doctest: +IGNORE_EXCEPTION_DETAIL
+""" result =>
+jsonschema.exceptions.ValidationError: 'Invalid' is not of type 'number'
+
+Failed validating 'type' in schema['properties']['price']:
+    {'type': 'number'}
+
+On instance['price']:
+    'Invalid'
+"""
