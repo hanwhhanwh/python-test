b36b304b101c9168f0161638f049009a974849c7
Wonhee <hanwhhanwh@gmail.com>
2021-08-11 16:56:19 +0900

    add From header

--- net/email/first_email.py
+++ net/email/first_email.py
@@ -1,14 +1,15 @@
 import smtplib
 from email.mime.text import MIMEText
  
-smtp = smtplib.SMTP('smtp.live.com', 587)
+smtp = smtplib.SMTP('mail.smtp-server.kr', 25)
 smtp.ehlo()      # say Hello
-smtp.starttls()  # TLS 사용시 필요
-smtp.login('hanwh@hunature.net', '**')
+#smtp.starttls()  # TLS 사용시 필요
+smtp.login('sender@smtp-server.kr', 'pasword@2!')
  
-msg = MIMEText('본문 테스트 메시지')
-msg['Subject'] = '메일 발송 시험 (2021.02.15)'
+msg = MIMEText('Mail contents test<br />test.. test..')
+msg['From'] = 'sender@smtp-server.kr'
 msg['To'] = 'hbesthee@naver.com'
-smtp.sendmail('hanwh@hunature.net', 'hbesthee@naver.com', msg.as_string())
+msg['Subject'] = '메일 발송 시험 (2021.08.05)'
+smtp.sendmail('sender@smtp-server.kr', 'hbesthee@naver.com', msg.as_string())
  
 smtp.quit()
