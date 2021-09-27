69a21a2ce155763865f7b601ad3100c24f5c7787
Wonhee Han <hanwhhanwh@gmail.com>
2021-09-27 15:48:59 +0900

    add basic socketio example

---
+++ http/flask/socketio/basic.py
@@ -0,0 +1,25 @@
+# reference : https://flask-socketio.readthedocs.io/en/latest/getting_started.html
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(data):
+	print('received message: ' + data)
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+	print('received json: ' + str(json))
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=80, use_reloader=False)
