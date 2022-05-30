56296efa75e4ccd75006ccebec84ac2daa334847
Wonhee Han <hanwhhanwh@gmail.com>
2022-05-31 08:56:51 +0900

    쿠다를 사용할 수 있는 장치 수

--- cv/cv_info.py
+++ cv/cv_info.py
@@ -7,7 +7,11 @@ print(cv2.__file__)
 # 패키지 버전 확인
 print(cv2.__version__)
 
+# 쿠다를 사용할 수 있는 장치 수
+print(cv2.cuda.getCudaEnabledDeviceCount())
+
 """ Result
 /opt/intel/openvino/python/python3/cv2/python-3/cv2.abi3.so
 4.5.3-openvino
+0
 """
