c9de23414d28f9e725a7956fbd5c15a59cff8837
Wonhee Han <hanwhhanwh@gmail.com>
2022-08-09 19:45:34 +0900

    receiving loop

--- general/serial_recv_loop.py
+++ general/serial_recv_loop.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
 m_serial = serial.Serial(port = 'COM11', baudrate = 115200, parity = serial.PARITY_NONE
 					, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS
-					, timeout = 1)
+					, timeout = 5)
 
 m_serial.write(PKT_SENSOR_INIT)
 m_serial.write(struct.pack(FMT_PKT_HEADER, PKT_PREFIX, CMD_SENSOR_INIT, 0))
@@ -63,20 +63,27 @@ while True:
 		continue
 
 	m_buffer += data
+	print(f'recv data = {data.hex()}')
 	buffer_len = len(m_buffer)
 	if (buffer_len >= PKT_HEADER_SIZE):
 		# analyze packet header
 		prefix_index = m_buffer.find(PKT_PREFIX_BYTES)
 		if (prefix_index == 0):
 			# found HEADER
-			print(f'recv data = {m_buffer.hex()}')
-			_, cmd_id, pkt_length = struct.unpack_from(FMT_PKT_HEADER, m_buffer, 0)
-			print(f'   cmd_id = {cmd_id:04x}, length = {pkt_length:08x}')
-			m_buffer = bytes()
-			pass
+			_, cmd_id, payload_length = struct.unpack_from(FMT_PKT_HEADER, m_buffer, 0)
+			packet_length = PKT_HEADER_SIZE + payload_length
+			if (buffer_len >= (packet_length)):
+				print(f'   cmd_id = {cmd_id:04x}, length = {payload_length:08x}')
+				pakcet = m_buffer[:packet_length]
+				print(f'recevied packet = {pakcet.hex()}')
+				m_buffer = m_buffer[packet_length:]
+			else:
+				continue
 		elif (prefix_index > 0):
 			# found PKT_PREFIX
-			pass
+			tresh_data = m_buffer[:prefix_index]
+			print(f'tresh data dropped ({prefix_index}) = {tresh_data.hex()}')
+			m_buffer = m_buffer[prefix_index:]
 		else:
 			# wrong data => clear buffer
 			m_buffer = bytes()
