26e4f50d9b31d29492e0dd5f9377a1e31458f078
Wonhee Han <hanwh@hunature.net>
2023-12-11 22:35:00 +0900

    배경에 동영상을 그려주는 예제

---
+++ pkg/PyQt5/paint_background_widget2.py
@@ -0,0 +1,73 @@
+# QWidget 배경 그리기 예제 1
+# make hbesthee@naver.com
+# date 2023-12-11
+
+import sys
+import cv2
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QMutex, QThread, pyqtSignal, QTimer
+from PyQt5.QtGui import QImage, QPainter, QPixmap
+from time import sleep
+
+
+class VideoThread(QThread):
+	changeFrame = pyqtSignal(QImage)
+
+	def __init__(self, bg_image: QImage, mutex: QMutex):
+		super().__init__()
+		self._bg_image = bg_image
+		self._mutex = mutex
+
+	def run(self):
+		# OpenCV를 사용하여 비디오 캡처 객체를 생성합니다.
+		cap = cv2.VideoCapture(r'D:\Temp\1654.mp4')
+		while True:
+			ret, frame = cap.read()
+			if ret:
+				# OpenCV에서 가져온 프레임을 QImage로 변환합니다.
+				rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+				h, w, ch = rgbImage.shape
+				bytesPerLine = ch * w
+				# 시그널을 통해 메인 스레드에 QImage 객체를 전달합니다.
+				self._mutex.lock()
+				new_frame = QImage(rgbImage.data, w, h, bytesPerLine, QImage.Format_RGB888)
+				self.changeFrame.emit(new_frame)
+				self._mutex.unlock()
+				sleep(0.033)
+
+class App(QWidget):
+	def __init__(self):
+		super().__init__()
+		self._bg_image: QImage = None
+		self._mutex: QMutex = QMutex()
+		self.initUI()
+		self.thread = VideoThread(self._bg_image, self._mutex)
+		self.thread.changeFrame.connect(self.setFrame)
+		self.thread.start()
+
+	def initUI(self):
+		self.setGeometry(300, 300, 400, 300)
+		self.setWindowTitle('OpenCV 프레임 예제')
+		self.show()
+
+	def paintEvent(self, event):
+		if (self._bg_image == None):
+			#super().paintEvent(event)
+			return
+		painter = QPainter()
+		painter.begin(self)
+		# QPainter를 사용하여 위젯의 배경에 이미지를 그립니다.
+		self._mutex.lock()
+		painter.drawImage(self.rect(), self._bg_image)
+		self._mutex.unlock()
+		painter.end()
+
+	def setFrame(self, new_frame: QImage):
+		self._bg_image = new_frame.copy()
+		self.update()
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = App()
+	sys.exit(app.exec_())
