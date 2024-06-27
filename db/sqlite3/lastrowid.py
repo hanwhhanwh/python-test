dab2522cb3ba08e7e24703a5102bc902a768b98c
Wonhee Han <hanwhhanwh@gmail.com>
2024-06-27 20:50:56 +0900

    MOD: fetchone()

--- db/sqlite3/lastrowid.py
+++ db/sqlite3/lastrowid.py
@@ -20,15 +20,28 @@ CREATE TABLE IF NOT EXISTS "HELLO" (
 cursor.execute(sql)
 print("Table created successfully.")
 
-sql = """INSERT OR IGNORE INTO "HELLO" ( "world" ) VALUES ( 'TEST4' ); """
+sql = """INSERT OR IGNORE INTO "HELLO" ( "world" ) VALUES ( 'TEST7' ); """
 cursor.execute(sql)
 hello_no = cursor.lastrowid
 print(f"last hello_no = {hello_no}")
 
 
+sql = """
+SELECT
+	hello_no
+FROM "HELLO"
+WHERE world = 'TEST'; """
+cursor.execute(sql)
+row = cursor.fetchone()
+print(f"{row[0]=}")
+
+
 #sql = """INSERT INTO "HELLO" ( "world" ) VALUES ( 'TEST' ); """
 # sqlite3.IntegrityError: UNIQUE constraint failed: HELLO.world
-sql = """INSERT OR IGNORE INTO "HELLO" ( "world" ) VALUES ( 'TEST' ); """
+# sql = """INSERT OR IGNORE INTO "HELLO" ( "world" ) VALUES ( 'TEST' ); """
+sql = """INSERT INTO "HELLO" ( "world" )
+SELECT 'TEST'
+WHERE NOT EXISTS ( SELECT 1 FROM "HELLO" WHERE world = 'TEST' ) ; """
 cursor.execute(sql)
 hello_no = cursor.lastrowid
 print(f"last hello_no = {hello_no}")
