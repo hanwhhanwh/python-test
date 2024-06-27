daf352e527d00642687cde2d2d38a3467bfa8a48
Wonhee Han <hanwhhanwh@gmail.com>
2024-06-27 20:40:40 +0900

    ADD: sqlite AUTOINCREMENT, lastrowid example

---
+++ db/sqlite3/lastrowid.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+# Connecting to sqlite ; if "hello.db" not exists, create "hello.db" file
+conn = sqlite3.connect("hello.db")
+print(conn)
+
+# Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+# Doping HELLO table if already exists.
+#cursor.execute("DROP TABLE IF EXISTS HELLO")
+
+# Creating table as per requirement
+sql = """
+CREATE TABLE IF NOT EXISTS "HELLO" (
+	"hello_no"	INTEGER PRIMARY KEY AUTOINCREMENT
+	, "world"	TEXT UNIQUE
+);"""
+
+cursor.execute(sql)
+print("Table created successfully.")
+
+sql = """INSERT OR IGNORE INTO "HELLO" ( "world" ) VALUES ( 'TEST4' ); """
+cursor.execute(sql)
+hello_no = cursor.lastrowid
+print(f"last hello_no = {hello_no}")
+
+
+#sql = """INSERT INTO "HELLO" ( "world" ) VALUES ( 'TEST' ); """
+# sqlite3.IntegrityError: UNIQUE constraint failed: HELLO.world
+sql = """INSERT OR IGNORE INTO "HELLO" ( "world" ) VALUES ( 'TEST' ); """
+cursor.execute(sql)
+hello_no = cursor.lastrowid
+print(f"last hello_no = {hello_no}")
+
+# Commit your changes in the database
+conn.commit()
+
+# Closing the connection
+conn.close()
